2138a4d788afc69697d5c538bdd89a6bec2cdef5
Luis Fernando Valera Bernal <neo121981@hotmail.com>
2025-06-15 22:57:26 +0200

    Se añaden las opciones de administracion de grupos

---
+++ src/Core/lfvb.secure.aplication/Database/Grupos/Queries/GetGrupo/GetGrupoQuery.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using lfvb.secure.aplication.Database.Aplicaciones.Models;
+using lfvb.secure.aplication.Database.Grupos.Models;
+using lfvb.secure.aplication.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lfvb.secure.aplication.Database.Grupos.Queries.GetGrupo
+{
+    public class GetGrupoQuery : IGetGrupoQuery
+    {
+        private readonly IDataBaseService _db;
+        private readonly IMapper _mapper;
+
+        public GetGrupoQuery(IDataBaseService db, IMapper mapper)
+        {
+            _db = db;
+            _mapper = mapper;
+        }
+
+
+        public async Task<GrupoModel?> Execute(Guid grupoId)
+        {
+            GrupoModel? grupo = await _db.Grupos.Include(p => p.Aplicacion)
+                .Where(g => g.Id == grupoId)
+                .Select(g => new GrupoModel
+                {
+                    Id = g.Id,
+                    Nombre = g.Nombre,
+                    Aplicacion = new AplicacionModel
+                    {
+                        Id = g.Aplicacion.Id,
+                        Codigo = g.Aplicacion.Codigo,
+                        Nombre = g.Aplicacion.Nombre
+                    }
+                })
+                .FirstOrDefaultAsync();
+            return grupo;
+        }
+    }
+}
---
+++ src/Core/lfvb.secure.aplication/Database/Grupos/Queries/GetUsuariosGrupo/GetUsuariosGrupoQuery.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using lfvb.secure.aplication.Database.Usuario.Models;
+using lfvb.secure.aplication.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lfvb.secure.aplication.Database.Grupos.Queries.GetUsuariosGrupo
+{
+    public class GetUsuariosGrupoQuery:IGetUsuariosGrupoQuery
+    {
+        private readonly IDataBaseService _db;
+        private readonly IMapper _mapper;
+
+        public GetUsuariosGrupoQuery(IDataBaseService db, IMapper mapper)
+        {
+            _db = db;
+            _mapper = mapper;
+        }
+
+
+        public async Task<List<UsuarioModel>> Execute(Guid grupoId)
+        {
+            List<UsuarioModel> usuarios = await (from u in _db.Usuarios.Include(x=>x.RelacionGrupos)
+                                                    where u.RelacionGrupos.Any(g => g.IdGrupo == grupoId)
+                                                    select new UsuarioModel
+                                                    {
+                                                        Id = u.Id,
+                                                        Nombre = u.Nombre,
+                                                        Apellido1 = u.Apellido1,
+                                                        Apellido2 = u.Apellido2,
+                                                        Email = u.Email,
+                                                        Usuario = u.Usuario
+                                                    }).ToListAsync<UsuarioModel>();
+
+            return usuarios;
+        }
+    }
+}
